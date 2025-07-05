554aa6b1c6d17f26648255ae53c326ce071791b2
Jesther Costinar <jesther.jc15@gmail.com>
2025-07-05 22:14:42 +0800

    Use pathVariable

--- src/main/java/com/springboot/firstapp/controller/StudentController.java
+++ src/main/java/com/springboot/firstapp/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.springboot.firstapp.controller;
 
 import com.springboot.firstapp.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -29,5 +30,11 @@ public class StudentController {
         students.add(new Student(3, "Dhen", "Test"));
 
         return students;
+    }
+
+    // Spring BOOT REST API with Path Variable
+    @GetMapping("students/{id}")
+    public Student studentPathVariable(@PathVariable("id") int studentId) {
+        return new Student(studentId, "Jes", "Costinar");
     }
 }
